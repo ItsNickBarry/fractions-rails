d278155c2f14532c7fac48836b2469569388349d
ItsNickBarry <itsnickbarry@protonmail.com>
2015-07-04 23:20:32 -0700

    install backbone-on-rails gem and generate backbone:install assets

---
+++ app/assets/javascripts/fractions.js
@@ -0,0 +1,13 @@
+window.Fractions = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Fractions.initialize();
+});
