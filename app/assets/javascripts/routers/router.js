13a6e658ee22c6724775e97bae0f4549374ecbb4
Nick Barry <itsnickbarry@protonmail.ch>
2016-05-13 18:56:56 -0400

    generalize action for show routes

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -2,13 +2,8 @@ Fractions.Routers.Router = Backbone.Router.extend({
   routes: {
     '':                            'root',
     'map':                         'mapShow',
-    'characters/:id':              'characterShow',
-    'electorates/:id':             'electorateShow',
+    ':object/:id':                 'objectShow',
     'fractions/:id/:nested_index': 'fractionNestedIndex',
-    'fractions/:id':               'fractionShow',
-    'positions/:id':               'positionShow',
-    'regions/:id':                 'regionShow',
-    'users/:id':                   'userShow',
   },
 
   initialize: function (options) {
@@ -20,50 +15,21 @@ Fractions.Routers.Router = Backbone.Router.extend({
     this.swapView(view);
   },
 
+  objectShow: function (object, id) {
+    var className = _.capitalize(_.camelize(_.singularize(object)));
+    var model = new Fractions.Models[className]({ id: id });
+    model.fetch({
+      success: function () {
+        var view = new Fractions.Views[className + 'Show']({ model: model });
+        this.swapView(view);
+      }.bind(this),
+    });
+  },
+
   mapShow: function () {
     // TODO pass objects into Map view?
     var view = new Fractions.Views.MapShow();
     this.swapView(view);
-  },
-
-  userShow: function (id) {
-    var model = new Fractions.Models.User({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.UserShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
-  },
-
-  characterShow: function (id) {
-    var model = new Fractions.Models.Character({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.CharacterShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
-  },
-
-  electorateShow: function (id) {
-    var model = new Fractions.Models.Electorate({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.ElectorateShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
-  },
-
-  fractionShow: function (id) {
-    var model = new Fractions.Models.Fraction({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.FractionShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
   },
 
   fractionNestedIndex: function (id, nested_index) {
@@ -80,29 +46,9 @@ Fractions.Routers.Router = Backbone.Router.extend({
     });
   },
 
-  positionShow: function (id) {
-    var model = new Fractions.Models.Position({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.PositionShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
-  },
-
-  regionShow: function (id) {
-    var model = new Fractions.Models.Region({ id: id });
-    model.fetch({
-      success: function () {
-        var view = new Fractions.Views.RegionShow({ model: model });
-        this.swapView(view);
-      }.bind(this),
-    });
-  },
-
   swapView: function (view) {
     this._currentView && this._currentView.remove();
     this._currentView = view;
     this.$pagesEl.html(view.render().$el);
-  }
+  },
 });
