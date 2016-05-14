c0928ae1006d80ed8ccdc30af712e404a5045d3e
Nick Barry <itsnickbarry@protonmail.ch>
2016-05-13 21:19:45 -0400

    add Associable Backbone concern

---
+++ app/assets/javascripts/concerns/associable.js
@@ -0,0 +1,59 @@
+Fractions.Concerns.Associable = {
+  belongsTo: function (association, options) {
+    options = options ? options : {};
+    var className = options.className || _.capitalize(association);
+    var privateName = '_' + association;
+    this[association] = function (polymorphic_type) {
+      if (!this[privateName]) {
+        options;
+        this[privateName] = new Fractions.Models[polymorphic_type || className]();
+      }
+      return this[privateName];
+    };
+    this._associations()[association] = {
+      polymorphic: options.polymorphic,
+      responseIndex: _.underscored(association),
+    };
+  },
+
+  hasMany: function (association, options) {
+    options = options ? options : {};
+    var className = options.className || _.capitalize(_.singularize(association));
+    var privateName = '_' + association;
+    this[association] = function (polymorphic_type) {
+      if (!this[privateName]) {
+        this[privateName] = new Fractions.Collections.NestedCollection({
+          model: Fractions.Models[className],
+          parentModel: this,
+          name: association,
+        });
+      }
+      return this[privateName];
+    };
+    this._associations()[association] = {
+      polymorphic: options.polymorphic,
+      responseIndex: _.underscored(association),
+    };
+  },
+
+  parse: function (response) {
+    _.each(this._associations(), function (options, name) {
+      var polymorphic_type;
+      if (options.polymorphic) {
+        polymorphic_type = response[name + '_type'];
+        delete response[name + '_type'];
+      }
+      this[name](polymorphic_type).set(response[options.responseIndex], { parse: true });
+      delete response[options.responseIndex];
+    }.bind(this));
+
+    return response;
+  },
+
+  _associations: function () {
+    if (typeof this._associationSet === 'undefined') {
+      this._associationSet = {};
+    }
+    return this._associationSet;
+  },
+};
--- app/assets/javascripts/models/fraction.js
+++ app/assets/javascripts/models/fraction.js
@@ -2,77 +2,22 @@ Fractions.Models.Fraction = Backbone.Model.extend(
   Fractions.Concerns.Governable
 ).extend(
   Fractions.Concerns.Routable
+).extend(
+  Fractions.Concerns.Associable
 ).extend({
 
   class: 'Fraction',
   urlFragmentRoot: '/fractions',
   urlRoot: '/api/fractions',
 
-  parse: function (response) {
-    if (response.founder) {
-      this.founder(response.founder_type).set(response.founder);
-      delete response.founder_type;
-      delete response.founder;
-    }
-    if (response.parent) {
-      this.parent().set(response.parent);
-      delete response.parent;
-    }
-    if (response.root) {
-      this.root().set(response.root);
-      delete response.root;
-    }
-
-    // parse and create accessor functions for NestedCollections
-    [
-      { name: 'electorates' },
-      { name: 'positions' },
-      { name: 'regions' },
-      { name: 'children', type: 'Fraction' },
-      { name: 'founded fractions', type: 'Fraction'}
-    ].forEach(function (object) {
-      var name = object.name;
-      var type = _.titleize(object.type || _.singularize(name));
-      var privateName = '_' + _.camelize(name);
-      var snakeName = _.underscored(name);
-      this[_.camelize(name)] = function () {
-        if (!this[privateName]) {
-          this[privateName] = new Fractions.Collections.NestedCollection({
-            model: Fractions.Models[type],
-            parentModel: this,
-            name: name,
-          });
-        }
-        return this[privateName];
-      };
-
-      // parse the collection
-      this[_.camelize(name)]().set(response[_.underscored(name)], { parse: true });
-      delete response[_.underscored(name)];
-
-    }.bind(this));
-
-    return response;
-  },
-
-  founder: function (founder_type) {
-    if (!this._founder && founder_type) {
-      this._founder = new Fractions.Models[founder_type]();
-    }
-    return this._founder;
-  },
-
-  parent: function () {
-    if (!this._parent) {
-      this._parent = new Fractions.Models.Fraction();
-    }
-    return this._parent;
-  },
-
-  root: function () {
-    if (!this._root) {
-      this._root = new Fractions.Models.Fraction();
-    }
-    return this._root;
+  initialize: function () {
+    this.belongsTo('founder', { polymorphic: true });
+    this.belongsTo('parent', { className: 'Fraction' });
+    this.belongsTo('root', { className: 'Fraction' });
+    this.hasMany('electorates');
+    this.hasMany('positions');
+    this.hasMany('regions');
+    this.hasMany('children', { className: 'Fraction' });
+    this.hasMany('foundedFractions', { className: 'Fraction' });
   },
 });
